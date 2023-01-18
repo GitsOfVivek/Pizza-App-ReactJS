4a6c9deaa2b20d13564e4e63b8661d60141750ff
GitsOfVivek <singhvivek1999@yahoo.com>
2023-01-18 15:25:02 +0530

    all files added

--- src/App.js
+++ src/App.js
@@ -1,25 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Navigation from './components/Navigation';
+import Cart from './pages/Cart';
+import Home from './pages/Home';
+import Products from './pages/Products';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	return (
+		<div className="w-4/5 mx-auto">
+			<Router>
+				<Navigation />
+				<Routes>
+					<Route path="/" element={<Home />}></Route>
+					<Route path="/products" element={<Products />}></Route>
+					<Route path="/cart" element={<Cart />}></Route>
+				</Routes>
+			</Router>
+		</div>
+	);
 }
 
 export default App;
